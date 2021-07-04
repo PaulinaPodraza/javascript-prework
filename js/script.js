358cbe8987ddebcff6aa6efc5d9dd228b15dcb92
Paulina Podraza <paulina.podraza92@gmail.com>
2021-07-04 12:09:47 +0200

    add code to script.js that shows the player's choice

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ if(randomNumber==1){
 } else {
 	printMessage('Mój ruch to: nożyce');
 }
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+	printMessage('papier');
+} else {
+	printMessage('nożyce');
+}
+
+printMessage('Twój ruch to: ' + playerMove);
