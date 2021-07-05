739f499d37c2baf17de6520ab760921babaf3a7f
Paulina Podraza <paulina.podraza92@gmail.com>
2021-07-05 21:24:02 +0200

    Add the getMoveName function and the displayResult function to the code in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-function printMessage(msg){
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  } else if(argMoveId == 2){
+    return 'papier';
+  } else if(argMoveId == 3){
+    return 'nożyce';
+  }
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function printMessage(msg) {
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
@@ -8,62 +21,45 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+let argMoveId = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + argMoveId);
 
-let computerMove = 'nieznany ruch';
-
-if (randomNumber == 1) {
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else {
-  computerMove = 'nożyce';
-}
-
-// printMessage('Wylosowana liczba to: ' + randomNumber);
-
-if(randomNumber==1){
-	printMessage('Mój ruch to: kamień');
-} else if (randomNumber == 2) {
-	printMessage('Mój ruch to: papier');
-} else {
-	printMessage('Mój ruch to: nożyce');
-}
+let argComputerMove = getMoveName(argMoveId);
+printMessage('Mój ruch to: ' + argComputerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let argPlayerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-	playerMove = 'papier';
-} else {
-	playerMove = 'nożyce';
+printMessage('Twój ruch to: ' + argPlayerMove);
+
+function displayResult(argComputerMove, argPlayerMove) {
+  printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove + ' wynik to : ');
+
+  if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+  printMessage('Ty wygrywasz');
+} else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+  printMessage('Ty wygrywasz');
+} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+  printMessage('Ty wygrywasz');
+} else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
+  printMessage('Remis');
+} else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+  printMessage('Remis');
+} else if (argComputerMove == 'papier' && argPlayerMove == 'papier'){
+  printMessage('Remis');
+} else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+  printMessage('Przegrałeś');
+} else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+  printMessage('Przegrałeś');
+} else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+  printMessage('Przegrałeś');
+  } else if (argComputerMove == 'nożyce', 'kamień', 'papier' && argPlayerMove == 'nieznany ruch'){
+  printMessage('Błąd');
+}
 }
 
-printMessage('Twój ruch to: ' + playerMove);
-
-if(computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ty wygrywasz');
-} else if (computerMove == 'papier' && playerMove == 'nożyce'){
-  printMessage('Ty wygrywasz');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
-  printMessage('Ty wygrywasz');
-} else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
-  printMessage('Remis');
-} else if (computerMove == 'kamień' && playerMove == 'kamień'){
-  printMessage('Remis');
-} else if (computerMove == 'papier' && playerMove == 'papier'){
-  printMessage('Remis');
-} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
-  printMessage('Przegrałeś');
-} else if (computerMove == 'papier' && playerMove == 'kamień'){
-  printMessage('Przegrałeś');
-} else if (computerMove == 'nożyce' && playerMove == 'papier'){
-  printMessage('Przegrałeś');
-}
+let wynik = displayResult(argComputerMove, argPlayerMove);
