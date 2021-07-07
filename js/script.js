c5ccc688ae8248cdd9567037c65fd2231095d309
Paulina Podraza <paulina.podraza92@gmail.com>
2021-07-07 18:54:21 +0200

    remove blank lines in the code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 function playGame(playerInput) {
-
-  clearMessages() ;
+  clearMessages();
 
     function getMoveName(argMoveId) {
       if (argMoveId == 1) {
@@ -21,30 +20,24 @@ function playGame(playerInput) {
       let div = document.createElement('div');
       div.innerHTML = msg;
       document.getElementById('messages').appendChild(div);
-    }
+  }
 
     function clearMessages() {
       document.getElementById('messages').innerHTML = '';
     }
 
     let argMoveId = Math.floor(Math.random() * 3 + 1);
-
     console.log('Wylosowana liczba to: ' + argMoveId);
 
     let argComputerMove = getMoveName(argMoveId);
     printMessage('Mój ruch to: ' + argComputerMove);
-
-    /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
-
     console.log('Gracz wpisał: ' + playerInput);
 
     let argPlayerMove = getMoveName(playerInput);
-
     printMessage('Twój ruch to: ' + argPlayerMove);
 
     function displayResult(argComputerMove, argPlayerMove) {
       printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove + ' wynik to: ');
-
       if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
         printMessage('Ty wygrywasz');
       } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
@@ -66,30 +59,30 @@ function playGame(playerInput) {
       } else if (argComputerMove == 'nożyce', 'kamień', 'papier' && argPlayerMove == 'nieznany ruch') {
         printMessage('Błąd');
       }
-    }
-
+  }
+  
     let wynik = displayResult(argComputerMove, argPlayerMove);
   }
 
 function buttonClicked1() {
   playGame('1');
 }
+
 let playRock = document.getElementById('play-rock');
-  
 playRock.addEventListener('click', buttonClicked1);
-  
+
 function buttonClicked2() {
   playGame('2');
 }
+
 let playPaper = document.getElementById('play-paper');
-  
 playPaper.addEventListener('click', buttonClicked2);
-  
+
 function buttonClicked3() {
   playGame('3');
 }
+
 let playScissors = document.getElementById('play-scissors');
-  
 playScissors.addEventListener('click', buttonClicked3);
 
 
